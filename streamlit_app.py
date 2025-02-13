c74c274a12900c596a32ebecf75196fe2ea45599
Mohamed S. Gabriel <82984368+MSMGabriel@users.noreply.github.com>
2025-02-13 10:50:57 +0000

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.set_page_config(page_title="News Sentiment App", page_icon="📰")
+
+st.title("🚧 News Sentiment App Will Be Back Soon! 🚧")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "We're working on improvements and will be back shortly! Stay tuned for updates. "
+    "If you need any assistance, please contact us at **Mohamed@concretumgroup.com** or **Carlo@concretumgroup.com**."
 )
