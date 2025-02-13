8f9bf4cc2681edd3fc7e24b1eb129ec0e05799e9
Mohamed S. Gabriel <82984368+MSMGabriel@users.noreply.github.com>
2025-02-13 10:51:34 +0000

    3rd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="News Sentiment App", page_icon="📰")
 
-st.title("🚧 News Sentiment App Will Be Back Soon! 🚧")
+st.title("News Sentiment App Will Be Back Soon!")
 st.write(
     "We're working on improvements and will be back shortly! Stay tuned for updates. "
     "If you need any assistance, please contact us at **Mohamed@concretumgroup.com** or **Carlo@concretumgroup.com**."
