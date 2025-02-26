a2c85f806f0d4050dd086559a157af106a7da72e
Mohamed S. Gabriel <82984368+MSMGabriel@users.noreply.github.com>
2025-02-26 14:48:47 +0000

    app is online

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="News Sentiment App", page_icon="📰")
 
-st.title("News Sentiment App Will Be Back Soon!")
+st.title("News Sentiment App is Back Online! 🎉")
 st.write(
-    "We're working on improvements and will be back shortly! Stay tuned for updates. "
+    "We're excited to announce that the News Sentiment App is now back online! "
+    "You can access it here: [News Sentiment App](https://news.concretumgroup.com/) "
     "If you need any assistance, please contact us at **Mohamed@concretumgroup.com** or **Carlo@concretumgroup.com**."
 )
